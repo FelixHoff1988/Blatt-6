24d9a5b3396ed4e083373b13daf3426942d06aef
FelixHoff1988 <*YKfM29QxNZS>
2022-11-24 18:56:56 +0100

    correction

--- A6_5.hs
+++ A6_5.hs
@@ -12,11 +12,11 @@ lft        :: BtreeFloat a -> BtreeFloat a
 rght       :: BtreeFloat a -> BtreeFloat a
 isLeaf      :: BtreeFloat a -> Bool
 
-isEmpty Nil = True                              --leerer Baum
-isEmpty (Node nValue tLeft tRight) = False      --nicht leerer Baum
+isEmpty Nil = True                             --leerer Baum
+isEmpty (Node nValue tLeft tRight) = False     --nicht leerer Baum
 
-value Nil = error "Baum ist leer"               --Zugriff nicht erlaubt
-value(Node nValue tLeft tRight) = nValue     --Liefert Wert des Knotens
+value Nil = error "Baum ist leer"              --Zugriff nicht erlaubt
+value(Node nValue tLeft tRight) = nValue       --Liefert Wert des Knotens
 
 lft Nil = error "Baum ist leer"                --Zugriff nicht erlaubt
 lft(Node nValue tLeft tRight) = tLeft          --linkes Kind
@@ -24,6 +24,6 @@ lft(Node nValue tLeft tRight) = tLeft          --linkes Kind
 rght Nil = error "Baum ist leer"               --Zugriff nicht erlaubt
 rght (Node nValue tLeft tRight) = tRight       --rechtes Kind
 
-isLeaf Nil = error "Baum ist leer"              --Zugriff nicht erlaubt
-isLeaf (Node nValue Nil Nil) = True             --Blatt hat keine Kinder
-isLeaf ( Node nValue tLeft tRight) = False      --sonst kein Blatt
+isLeaf Nil = error "Baum ist leer"             --Zugriff nicht erlaubt
+isLeaf (Node nValue Nil Nil) = True            --Blatt hat keine Kinder
+isLeaf ( Node nValue tLeft tRight) = False     --sonst kein Blatt
